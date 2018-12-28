ec83f1eb41f7695ffd153ba4306300ff43730d76
ADMIN <ADMIN@HP>
2018-12-28 11:37:23 +0530

    Third Commit

---
+++ src/main/java/spring_jdbc/com/hexa/client/ClientApp2.java
@@ -0,0 +1,10 @@
+package spring_jdbc.com.hexa.client;
+
+public class ClientApp2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     System.out.println("gjghjghjg");
+	}
+
+}
